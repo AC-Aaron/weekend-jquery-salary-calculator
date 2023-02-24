a6a4727d66c475518082130183272f8247da182c
Aaron Aguilar-Chavez <oyorom8@gmail.com>
2023-02-24 16:35:54 -0600

    last few tweeks before leaving class

--- myscript.js
+++ myscript.js
@@ -1,2 +1,55 @@
 console.log('JS is sourced, you may proceed')
-alert('JS is sourced, you may proceed');
+
+$(document).ready(readyNow);
+
+function readyNow() {
+    console.log('DOM is loaded!!');
+    $('#infoSubmitButton').on('click', submitEmployeeInfo);
+    
+}
+
+//create an array to hold info obtained from field inputs
+let employeeFormInputs = [];
+
+//array items should be an object with the follwing properties:
+/*  -employee firstname:
+    -employee lastname:
+    -ID number:
+    -job title:
+    -annual salary:
+*/
+
+function submitEmployeeInfo() {
+console.log('hey, you clicked the submit button!!') 
+   
+    //creating variables to store values from the input fields:
+    const employeeFirstName = $('#firstNameInput').val();
+    const employeeLastName = $('#lastNameInput').val();
+    const employeeIDnumber = $('#idNumberInput').val();
+    const employeeJobTitle = $('#titleInput').val();
+    const employeeAnnualSalary = $('#annualSalaryInput').val();
+
+    //create a new object from the input fields with following properties:
+    // -employee lastname:
+    // -ID number:
+    // -job title:
+    // -annual salary:
+    let newFormInputs = {
+        firstName:employeeFirstName,
+        lastName:employeeLastName,
+        IDnumber:employeeIDnumber,
+        JobTitle:employeeJobTitle,
+        AnnualSalary:employeeAnnualSalary
+    }
+    //add new object to the employeeFormInputs array
+    employeeFormInputs.push(newFormInputs);     
+}
+
+/*
+A 'Submit' button should collect the form information, store the information 
+to calculate monthly costs, append information to the DOM and clear the input 
+fields. Using the stored information, calculate monthly costs and append this
+ to the to DOM. If the total monthly cost exceeds $20,000, add a red 
+ background color to the total monthly cost.
+*/
+
